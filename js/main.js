229757c401a5874e5e3e9b16e86e9fa2e3f1bd6e
Mary Minikh <maryminikh28@gmail.com>
2023-09-07 11:26:28 +0300

    ДОРАБОТКА: попытка в тесты, функция старт вынесена за мейн

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ function input() {
   });
 }
 
-// todo Z - кол-во рандомных значений
+// Z - кол-во рандомных значений
 function FNR(Z) {
   return Math.floor(Z * Math.random());
 }
@@ -69,8 +69,89 @@ const data_sound = [
 // let T_tries = 10000000000;
 
 let K_array_of_right_keys = new Array(20);
+let D_current_door = 1;
+
+const StartGame = async () => {
+  let N_locked_doors = 3 + FNR(3);
+  let K3_keys = 8 + FNR(5);
+  let T_tries = 10 + FNR(21);
+
+  print("\n");
+  print(`THERE ARE ${N_locked_doors} LOCKED DOORS AND THERE ARE ${K3_keys} KEYS (0-${K3_keys - 1})\n`);
+  print(`YOU WILL HAVE ${T_tries - 1} TRIES TO OPEN THEM ALL\n`);
+  print(`(SOME KEYS MAY OPEN MORE THAN ONE DOOR)\n\n`);
+
+  //todo заполнение массива
+  for (let x = 1; x <= N_locked_doors; x++) {
+    // K_array_of_right_keys.fill(FNR(K3_keys));
+    // K_array_of_right_keys.push(FNR(K3_keys));
+    K_array_of_right_keys[x] = FNR(K3_keys);
+
+    console.log('array', K_array_of_right_keys)
+  }
+
+  T_tries--;
+
+  // текущая попытка
+  while (T_tries > 0) {
+    print(`TRIES LEFT = ${T_tries}       DOOR #${D_current_door}  KEY`)
+    const K2_chosen_key = await input();
+
+    if (K2_chosen_key != K_array_of_right_keys[D_current_door]) {
+      T_tries--;
+      if (T_tries == 0) {
+        console.log('out of tries')
+
+        break;
+      }
+      console.log('wrong key:', K2_chosen_key, 'right:', K_array_of_right_keys[D_current_door])
+
+      continue;
+    }
+
+    print(`${data_sound[FNR(7)]} \n`);
+    D_current_door++;
+
+    if (D_current_door > N_locked_doors) {
+      break;
+    }
+  }
+
+  print("\n");
+  if (D_current_door === N_locked_doors + 1) {
+    print(`YOU DID IT, BEHIND DOOR #${N_locked_doors} IS...........................\n`);
+    print(`${data_content[FNR(7)]} !!\n`);
+
+    await Question();
+
+  } else {
+    print(`YOU LOSE,  THE REST OF THE KEYS ARE:\n`);
+    for (let X = D_current_door; X <= N_locked_doors; X++) {
+      print(`\nDOOR ${X} \n KEY ${K_array_of_right_keys[X]}  \n`);
+    }
+
+    await Question();
+  }
+}
+
+async function Question() {
+  print(`\nDO YOU WANT TO PLAY AGAIN (YES SIR! OR NO SIR)!\n`)
+
+  let Q = await input();
+
+  if (Q === "YES SIR!") {
+    await StartGame();
+  }
+  if (Q === "NO SIR!") {
+    return 0;
+  } else {
+    print(`\nHEY, I DIDN'T JUST FALL OFF A TURNIP TRUCK, YA KNOW!!!!!\n`);
+    await Question();
+  }
+}
 
 async function main() {
+
   print(tab(27) + "DOORS\n");
   print(tab(20) + "CREATIVE COMPUTING\n");
   print(tab(18) + "MORRISTOWN, NEW JERSEY\n");
@@ -78,85 +159,7 @@ async function main() {
   print("\n");
   print("\n");
 
-  await start();
-
-  async function start() {
-    let N_locked_doors = 3 + FNR(3);
-    let K3_keys = 8 + FNR(5);
-    let T_tries = 10 + FNR(21);
-    let D_current_door = 1;
-
-    print("\n");
-    print(`THERE ARE ${N_locked_doors} LOCKED DOORS AND THERE ARE ${K3_keys} KEYS (0-${K3_keys - 1})\n`);
-    print(`YOU WILL HAVE ${T_tries - 1} TRIES TO OPEN THEM ALL\n`);
-    print(`(SOME KEYS MAY OPEN MORE THAN ONE DOOR)\n\n`);
-
-    //todo заполнение массива
-    for (let x = 1; x <= N_locked_doors; x++) {
-      // K_array_of_right_keys.fill(FNR(K3_keys));
-      // K_array_of_right_keys.push(FNR(K3_keys));
-      K_array_of_right_keys[x] = FNR(K3_keys);
-
-      console.log('array', K_array_of_right_keys)
-    }
-
-    T_tries--;
-
-    // текущая попытка
-    while (T_tries > 0) {
-      print(`TRIES LEFT = ${T_tries}       DOOR #${D_current_door}  KEY`)
-      const K2_chosen_key = await input();
-
-      if (K2_chosen_key != K_array_of_right_keys[D_current_door]) {
-        T_tries--;
-        if (T_tries == 0) {
-          console.log('out of tries')
-
-          break;
-        }
-        console.log('wrong key:', K2_chosen_key, 'right:', K_array_of_right_keys[D_current_door])
-
-        continue;
-      }
-
-      print(`${data_sound[FNR(7)]} \n`);
-      D_current_door++;
-
-      if (D_current_door > N_locked_doors) {
-        break;
-      }
-    }
-
-    print("\n");
-    if (D_current_door === N_locked_doors + 1) {
-      print(`YOU DID IT, BEHIND DOOR #${N_locked_doors} IS...........................\n`);
-      print(`${data_content[FNR(7)]} !!\n`);
-
-      await Question();
-
-    } else {
-      print(`YOU LOSE,  THE REST OF THE KEYS ARE:\n`);
-      for (let X = D_current_door; X <= N_locked_doors; X++) {
-        print(`\nDOOR ${X} \n KEY ${K_array_of_right_keys[X]}  \n`);
-      }
-
-      await Question();
-    }
-  }
-
-  async function Question() {
-    print(`\nDO YOU WANT TO PLAY AGAIN (YES SIR! OR NO SIR!\n`)
-    let Q = await input();
-    if (Q === "YES SIR!") {
-      await start();
-    }
-    if (Q === "NO SIR!") {
-      return 0;
-    } else {
-      print(`\nHEY, I DIDN'T JUST FALL OFF A TURNIP TRUCK, YA KNOW!!!!!\n`);
-      await Question();
-    }
-  }
+  await StartGame();
 }
 
 main();
